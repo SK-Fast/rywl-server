ad369912d2cf634602d0251b1a566849715bf717
devy <60780448+SK-Fast@users.noreply.github.com>
2023-11-02 23:04:01 +0700

    run

--- setup.js
+++ setup.js
@@ -1,4 +1,5 @@
 import fs from 'fs'
+try { fs.rmSync("cache", {recursive: true}) } catch{}
 try { fs.mkdirSync("cache") } catch{}
 try { fs.writeFileSync("cache/announcement-cache.json", "[]") } catch{}
 try { fs.writeFileSync("cache/banners-cache.json", "[]") } catch{}
