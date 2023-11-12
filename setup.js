438918dd692f17591b0035e43a03d4dc86eea61c
devy <60780448+SK-Fast@users.noreply.github.com>
2023-11-12 22:19:37 +0700

    setup files

--- setup.js
+++ setup.js
@@ -3,3 +3,18 @@ try { fs.rmSync("cache", {recursive: true}) } catch{}
 try { fs.mkdirSync("cache") } catch{}
 try { fs.writeFileSync("cache/announcement-cache.json", "[]") } catch{}
 try { fs.writeFileSync("cache/banners-cache.json", "[]") } catch{}
+try { fs.writeFileSync("cache/banners-cache.json", "[]") } catch{}
+try { fs.writeFileSync("/etc/systemd/system/rywld.service", `
+[Unit]
+Description=RYWL Service
+After=network.target
+StartLimitIntervalSec=0
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=1
+ExecStart=node /rywl-server/index.js
+
+[Install]
+WantedBy=multi-user.target`) } catch(e) {console.log(e)}
